183fc52c3f9a775fefc203ac2ed31ced9b0ea87a
ebrand0007 <ebrand@edslt3.area52.com>
2017-07-07 11:52:48 -0400

    fixed src/lino_base_node.cpp odom_frame typo

--- src/lino_base_node.cpp
+++ src/lino_base_node.cpp
@@ -144,7 +144,7 @@ int main(int argc, char** argv){
 
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
-    odom.header.frame_id = odom_topic;
+    odom.header.frame_id = odom_frame;
     //robot's position in x,y, and z
     odom.pose.pose.position.x = x_pos;
     odom.pose.pose.position.y = y_pos;
