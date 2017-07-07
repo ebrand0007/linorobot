46b8c5f44e0b723f23b67f093198308d69d140ef
ebrand0007 <ebrand@edslt3.area52.com>
2017-07-07 09:47:41 -0400

    Updated to lates upstream, excluding arduino code, and added ros params to linorobot_base

--- src/lino_base_node.cpp
+++ src/lino_base_node.cpp
@@ -1,119 +1,137 @@
 /*
  Copyright (c) 2016, Juan Jimeno
  Source: http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
- All rights reserved. 
- 
- Redistribution and use in source and binary forms, with or without 
- modification, are permitted provided that the following conditions are met: 
- 
- * Redistributions of source code must retain the above copyright notice, 
- this list of conditions and the following disclaimer. 
- * Redistributions in binary form must reproduce the above copyright 
- notice, this list of conditions and the following disclaimer in the 
- documentation and/or other materials provided with the distribution. 
- * Neither the name of  nor the names of its contributors may be used to 
- endorse or promote products derived from this software without specific 
- prior written permission. 
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- POSSIBILITY OF SUCH DAMAGE. 
- 
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of  nor the names of its contributors may be used to
+ endorse or promote products derived from this software without specific
+ prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Vector3.h>
+#include <lino_msgs/Velocities.h>
 #include <sensor_msgs/Imu.h>
 #include <math.h>
 
-double raw_vel = 0.0;
-double vel_dt = 0.0;
-double imu_z = 0.0;
+double g_vel_x = 0.0;
+double g_vel_y = 0.0;
 
-ros::Time current_time;
-ros::Time vel_time(0.0);
-ros::Time last_time(0.0);
+double g_vel_dt = 0.0;
+double g_imu_dt = 0.0;
+double g_imu_z = 0.0;
 
-void vel_callback( const geometry_msgs::Vector3Stamped& vel) {
+ros::Time g_last_loop_time(0.0);
+ros::Time g_last_vel_time(0.0);
+ros::Time g_last_imu_time(0.0);
+
+void velCallback( const lino_msgs::Velocities& vel) {
   //callback every time the robot's linear velocity is received
-  raw_vel = vel.vector.x;
-  vel_dt = vel.vector.z;
-  vel_time = vel.header.stamp;
+  ros::Time current_time = ros::Time::now();
+
+  g_vel_x = vel.linear_x;
+  g_vel_y = vel.linear_y;
+
+  g_vel_dt = (current_time - g_last_vel_time).toSec();
+  g_last_vel_time = current_time;
 }
 
-void imu_callback( const sensor_msgs::Imu& imu){
+void IMUCallback( const sensor_msgs::Imu& imu){
   //callback every time the robot's angular velocity is received
-  
+  ros::Time current_time = ros::Time::now();
   //this block is to filter out imu noise
-  if(imu.angular_velocity.z > -0.005 && imu.angular_velocity.z < 0) 
+  if(imu.angular_velocity.z > -0.03 && imu.angular_velocity.z < 0.03)
   {
-    imu_z = 0.00;
+    g_imu_z = 0.00;
   }
   else
   {
-    imu_z = imu.angular_velocity.z;
+    g_imu_z = imu.angular_velocity.z;
   }
+
+  g_imu_dt = (current_time - g_last_imu_time).toSec();
+  g_last_imu_time = current_time;
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "base_controller");
   ros::NodeHandle n;
   ros::NodeHandle nh_private_("~");
-  ros::Subscriber sub = n.subscribe("raw_vel", 50, vel_callback);
-  ros::Subscriber imu_sub = n.subscribe("imu/data", 50, imu_callback);
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  
+  //lauch params
+  std::string baselink_frame;
+  std::string odom_frame;
+  std::string imu_topic;
+  std::string vel_topic;
+  n.param<std::string>("baselink_frame", baselink_frame, "base_link");
+  n.param<std::string>("odom_frame", odom_frame, "odom");
+  n.param<std::string>("imu_topic", imu_topic, "imu/data");
+  n.param<std::string>("vel_topic", vel_topic, "raw_vel");
+
+  //ros::Subscriber sub = n.subscribe("raw_vel", 50, velCallback);
+  //ros::Subscriber imu_sub = n.subscribe("imu/data", 50, IMUCallback);
+  //ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  ros::Subscriber sub = n.subscribe(vel_topic, 50, velCallback);
+  ros::Subscriber imu_sub = n.subscribe(imu_topic, 50, IMUCallback);
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>(odom_frame, 50);
   tf::TransformBroadcaster odom_broadcaster;
 
   double rate = 10.0;
-  double dt = 0.0;
   double x_pos = 0.0;
   double y_pos = 0.0;
   double theta = 0.0;
-
   ros::Rate r(rate);
   while(n.ok()){
     ros::spinOnce();
-    current_time = ros::Time::now();
-       
-    dt = vel_dt;
-    //calculate change in time (dt)
-    double dtt = (current_time - last_time).toSec();
+    ros::Time current_time = ros::Time::now();
 
-    //linear velocity is the linear velocity published from the Teensy board(raw_vel)
-    double linear_velocity = raw_vel;
+    //linear velocity is the linear velocity published from the Teensy board in x axis
+    double linear_velocity_x = g_vel_x;
+
+    //linear velocity is the linear velocity published from the Teensy board in y axis
+    double linear_velocity_y = g_vel_y;
+
     //angular velocity is the rotation in Z from imu_filter_madgwick's output
-    double angular_velocity = imu_z;
-    
-    //calculate angular displacement  θ = ω * t
-    double delta_theta = angular_velocity * dtt; //radians
-    //calculate linear displacement in X axis
-    double delta_x = (linear_velocity * cos(theta)) * dt; //m
-    //calculate linear displacement in Y axis
-    double delta_y = (linear_velocity * sin(theta)) * dt; //m
+    double angular_velocity = g_imu_z;
 
-    //calculate current position of the robot 
-    //where (x,y) is summation of linear and angular displacement
+    //calculate angular displacement  θ = ω * t
+    double delta_theta = angular_velocity * g_imu_dt; //radians
+    double delta_x = (linear_velocity_x * cos(theta) - linear_velocity_y * sin(theta)) * g_vel_dt; //m
+    double delta_y = (linear_velocity_x * sin(theta) + linear_velocity_y * cos(theta)) * g_vel_dt; //m
+
+    //calculate current position of the robot
     x_pos += delta_x;
     y_pos += delta_y;
     theta += delta_theta;
 
-    //calculate robot's heading in quaternion angle
+    //calculate robot's heading in quarternion angle
     //ROS has a function to calculate yaw in quaternion angle
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
-    
+
     geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
+    odom_trans.header.frame_id = odom_frame;
+    odom_trans.child_frame_id = baselink_frame;
     //robot's position in x,y, and z
     odom_trans.transform.translation.x = x_pos;
     odom_trans.transform.translation.y = y_pos;
@@ -123,32 +141,33 @@ int main(int argc, char** argv){
     odom_trans.header.stamp = current_time;
     //publish robot's tf using odom_trans object
     odom_broadcaster.sendTransform(odom_trans);
-    
-    
+
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
+    odom.header.frame_id = odom_topic;
     //robot's position in x,y, and z
     odom.pose.pose.position.x = x_pos;
     odom.pose.pose.position.y = y_pos;
     odom.pose.pose.position.z = 0.0;
     //robot's heading in quaternion
     odom.pose.pose.orientation = odom_quat;
-  
-    odom.child_frame_id = "base_link";
+
+    odom.child_frame_id = baselink_frame;
     //linear speed from encoders
-    odom.twist.twist.linear.x = linear_velocity;
-    odom.twist.twist.linear.y = 0.0;
+    odom.twist.twist.linear.x = linear_velocity_x;
+    odom.twist.twist.linear.y = linear_velocity_y;
     odom.twist.twist.linear.z = 0.0;
-    
+
     odom.twist.twist.angular.x = 0.0;
     odom.twist.twist.angular.y = 0.0;
     //angular speed from IMU
-    odom.twist.twist.angular.z = imu_z;
+    odom.twist.twist.angular.z = g_imu_z;
+
+    //TODO: include covariance matrix here
 
     odom_pub.publish(odom);
-    
-    last_time = current_time;
+
+    g_last_loop_time = current_time;
     r.sleep();
   }
 }
